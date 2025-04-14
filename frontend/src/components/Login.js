215a6fe61ac993c62eae9e6565e992c6204ff6f7
Thabiso Mahlaola <2426592@students.wits.ac.za>
2025-04-14 13:37:00 +0200

    clean-up login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {  Alert } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext";
 import { Container, Row, Col } from "react-bootstrap";
 import "../styles/login.css";
 
+// // FontAwesome imports
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const { logIn, googleSignIn } = useUserAuth();
   const navigate = useNavigate();
 
-
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setError("");
+  //   try {
+  //     await logIn(email, password);
+  //     navigate("/home");
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
   const handleGoogleSignIn = async (e) => {
     e.preventDefault();
@@ -21,11 +35,12 @@ const Login = () => {
       const user = await googleSignIn();
       const userEmail = user.user.email;
       setEmail(userEmail);
-      Login = userEmail;
-      
       navigate("/home");
+      await logIn(email, password);
+      setPassword("Hello");
     } catch (error) {
       console.log(error.message);
+      setError(error.message);
     }
   };
 
@@ -37,13 +52,42 @@ const Login = () => {
             <h1>Login</h1>
             {error && <Alert variant="danger">{error}</Alert>}
 
+            {/* <Form onSubmit={handleSubmit}>
+              <div className="input-box">
+                <Form.Control
+                  type="email"
+                  placeholder="Email address"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <i><FontAwesomeIcon icon={faEnvelope} /></i>
+              </div>
+
+              <div className="input-box">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <i><FontAwesomeIcon icon={faLock} /></i>
+              </div>
+
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" /> Remember me
+                </label>
+                <a href="#">Forgot Password?</a>
+              </div>
+
+              <button className="btn" type="submit">
+                Log In
+              </button>
+            </Form> */}
 
             <div className="register-link">
               <p>
                 Don't have an account? 
               </p>
             </div>
-
             <GoogleButton className="g-btn" type="dark" onClick={handleGoogleSignIn} />
 
             
