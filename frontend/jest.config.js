a0bebcf28d4d3e935344d1aa5603643726706037
Thabiso Mahlaola <2426592@students.wits.ac.za>
2025-04-23 10:41:37 +0200

    code coverage v1

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-    collectCoverage: true,
-    coverageReporters: ['text', 'cobertura'],
-    moduleNameMapper: {
-        '\\.css$': 'identity-obj-proxy',
-        
-      },
-      transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-        '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': 'jest-transform-stub',
-      },
-      testEnvironment: 'jsdom',
-      
-}
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["cobertura", "text"],
+  testEnvironment: "jsdom",
+};
