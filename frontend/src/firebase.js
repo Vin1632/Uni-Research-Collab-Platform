28300735d3ab8268557bba5cfc5c807fc2a6dccf
Thabiso Mahlaola <2426592@students.wits.ac.za>
2025-04-29 13:30:39 +0200

    config-info/no_need_firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,25 +6,17 @@ import { getStorage } from "firebase/storage"; //Storing files inside firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-require('dotenv').config();
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDwJ8KhcJu4RoNI9X1NB1-JvBTvrPSDL4A",
-//   authDomain: "uni-research-collab.firebaseapp.com",
-//   projectId: "uni-research-collab",
-//   storageBucket: "uni-research-collab.firebasestorage.app",
-//   messagingSenderId: "342563407789",
-//   appId: "1:342563407789:web:1f2e8400332ed09a104324"
-// };
 
+//Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId:process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyDwJ8KhcJu4RoNI9X1NB1-JvBTvrPSDL4A",
+  authDomain: "uni-research-collab.firebaseapp.com",
+  projectId: "uni-research-collab",
+  storageBucket: "uni-research-collab.firebasestorage.app",
+  messagingSenderId: "342563407789",
+  appId: "1:342563407789:web:1f2e8400332ed09a104324"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app)
