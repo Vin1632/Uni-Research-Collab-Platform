f4f3ab659a8bd16b56f8027aab5980283dc46977
Thabiso Mahlaola <2426592@students.wits.ac.za>
2025-05-13 18:50:20 +0200

    build in production

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Sample API endpoint
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from The Thabiso!' });
+  res.json({ message: 'Hello THis is the new  World!' });
 });
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
